e335d064de570e49ffea67a6db04e507cb68889c
Sergei Shevchenko <sns@itqua.ru>
2022-03-17 22:40:32 +1000

    +

--- resources/views/paydocs/rep53.blade.php
+++ resources/views/paydocs/rep53.blade.php
@@ -172,9 +172,9 @@ $thisTitle = $report->title ?? $report->name;
                             $sh_qty = (isset($rec->qty)) ? number_format($rec->qty, 2) : '';
                             $sh_price = (isset($rec->price)) ? number_format($rec->price, 2) : '';
 
-                            if ($rec->sysobjid == 520)
-                                $ref_url = route('paydocs.edit', $rec->objid);
-                            else
+//                            if ($rec->sysobjid == 520)
+//                                $ref_url = route('paydocs.edit', $rec->objid);
+//                            else
                                 $ref_url = null;
 
                             $tstyle = ($rec->sumtypeid == 1) ? 'background-color:#ffff94' : '';
