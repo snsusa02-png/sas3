002de39e59951417928f2801424d97d191e459d7
Sergei Shevchenko <sns@itqua.ru>
2022-03-19 13:52:20 +1000

    + tbl proj_categories

---
+++ app/Http/Controllers/ProjCategoryController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\proj_category;
+use Illuminate\Http\Request;
+
+class ProjCategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\proj_category  $proj_category
+     * @return \Illuminate\Http\Response
+     */
+    public function show(proj_category $proj_category)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\proj_category  $proj_category
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(proj_category $proj_category)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\proj_category  $proj_category
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, proj_category $proj_category)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\proj_category  $proj_category
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(proj_category $proj_category)
+    {
+        //
+    }
+}
