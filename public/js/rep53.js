da0182f9f84c695bfc51b9bc25f2a403f9033db4
Sergei Shevchenko <sns@itqua.ru>
2022-04-23 15:49:11 +1000

    ++

--- public/js/rep53.js
+++ public/js/rep53.js
@@ -6,14 +6,15 @@ $(document).ready(function () {
         else
             $(this).addClass('bg-warning clicked');
 
-        cnt = totSum = 0;
+        let cnt = 0;
+        let totSum = 0;
         $('.clicked').each(function (i, obj) {
             cnt++
             totSum += parseFloat($(obj).data('num'));
         });
         if (cnt > 0) {
-            $("#_calc_selected_sum").html('sum: ' + totSum)
-            $("#_calc_selected_sum").show()
+            $("#_calc_selected_sum").html('sum: ' + totSum).show()
+            //$("#_calc_selected_sum").show()
         } else {
             $("#_calc_selected_sum").hide()
         }
@@ -22,16 +23,16 @@ $(document).ready(function () {
 
 
 function convertDate(d) {
-    var p = d.split(".");
+    let p = d.split(".");
     //console.log(parseInt(p[2])*10000+ parseInt(p[1])*100 + parseInt(p[0]))
     //return +(p[2] + p[1] + p[0]);
     return +(parseInt(p[2]) * 10000 + parseInt(p[1]) * 100 + parseInt(p[0]));
 }
 
 function sortByDate() {
-    var tbody = document.querySelector("#results tbody");
+    let tbody = document.querySelector("#results tbody");
     // get trs as array for ease of use
-    var rows = [].slice.call(tbody.querySelectorAll("tr"));
+    let rows = [].slice.call(tbody.querySelectorAll("tr"));
 
     rows.sort(function (a, b) {
         console.log(convertDate(a.cells[0].innerHTML))
@@ -45,17 +46,17 @@ function sortByDate() {
 }
 
 function sortByNPP() {
-    var tbody = document.querySelector("#results tbody");
+    let tbody = document.querySelector("#results tbody");
     // получим все строки таблицы как массив
-    var rows = [].slice.call(tbody.querySelectorAll("tr"));
-    var sort_btn = document.querySelector("#sort_1");
-    var sort_dir = -1*sort_btn.dataset.dir;
-    if (sort_dir == 1) {
+    let rows = [].slice.call(tbody.querySelectorAll("tr"));
+    let sort_btn = document.querySelector("#sort_1");
+    let sort_dir = -1*parseInt(sort_btn.dataset.dir);
+    if (sort_dir === 1) {
         sort_btn.innerHTML = '<i class="fa fa-sort-desc" aria-hidden="true"></i>';
     } else{
         sort_btn.innerHTML = '<i class="fa fa-sort-asc" aria-hidden="true"></i>';
     }
-    sort_btn.dataset.dir = sort_dir;
+    sort_btn.dataset.dir = sort_dir.toString();
 
     rows.sort(function (a, b) {
         return sort_dir * ((a.cells[0].dataset.npp) - (b.cells[0].dataset.npp));
@@ -68,5 +69,5 @@ function sortByNPP() {
 
 //document.querySelector("#sort_1").addEventListener("click", sortByDate);
 document.querySelector("#sort_1").addEventListener("click", sortByNPP);
-document.querySelector("#sort_1").addEventListener("touchend", sortByNPP);
+//document.querySelector("#sort_1").addEventListener("touchend", sortByNPP);
 sortByNPP();
