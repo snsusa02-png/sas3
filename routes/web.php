49e79b88ed707008d6af4c59aa53fb80b698899c
Sergei Shevchenko <sns@itqua.ru>
2024-09-01 18:02:41 +1000

    +

--- routes/web.php
+++ routes/web.php
@@ -1135,6 +1135,7 @@ Route::match(array('POST', 'GET'), '/reports/rep/59', "DriverWorkReportControlle
 Route::match(array('POST', 'GET'), '/reports/rep/60', "WrhDocReportController@rep60")->name('reports.rep60');
 Route::match(array('POST', 'GET'), '/reports/rep/61', "MchnRaidReportController@rep61")->name('reports.rep61');
 Route::match(array('POST', 'GET'), '/reports/rep/62', "OrgChargeController@rep62")->name('reports.rep62');
+Route::match(array('POST', 'GET'), '/reports/rep/63', "OrgStaffReportController@rep63")->name('reports.rep63');
 
 //Состав плана платежей
 Route::get('orgplnpay_items/create/{docid}/', "OrgplnpayItemController@create")->name('orgplnpay_items.create');
