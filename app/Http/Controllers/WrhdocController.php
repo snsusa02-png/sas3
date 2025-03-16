5e0cde3dcb85196facc0f0b9d96d3acee3969694
Sergei Shevchenko <sns@itqua.ru>
2025-03-16 13:01:34 +1000

    -

--- app/Http/Controllers/WrhdocController.php
+++ app/Http/Controllers/WrhdocController.php
@@ -1352,7 +1352,7 @@ class WrhdocController extends Controller
                 ->groupby('ci.refitmid')
 //                ->toSql();
                 ->get();
-//            dd($items);
+            dd($items);
 
             //пометим текущие записи состава обновляемого документа через updated_by=0
             wrhdoclst::where('docid', $doc->id)->update(['updated_by' => 0]);
