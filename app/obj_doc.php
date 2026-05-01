1187281958d3ff1c08ea8f4cc7b8f173b840780e
Sergei Shevchenko <sns@itqua.ru>
2026-05-01 19:21:28 +1000

    +

---
+++ app/obj_doc.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App;
+
+use App\Traits\DeleteTrait;
+use Illuminate\Database\Eloquent\Model;
+
+class obj_doc extends Model
+{
+    use DeleteTrait;
+
+    static public $prefix = 'obj_docs';
+    static public $sysobjid = 923;
+
+    protected $guarded = [];
+
+    public function sysobj()
+    {
+        return $this->hasOne(sysobj::class, 'id', 'sysobjid');
+    }
+
+    public function doctype()
+    {
+        return $this->hasOne(doctype::class, 'id', 'doctypeid');
+    }
+
+    public function whocrt()
+    {
+        return $this->hasOne(User::class, 'id', 'created_by');
+    }
+
+    public function whoupd()
+    {
+        return $this->hasOne(User::class, 'id', 'updated_by');
+    }
+
+
+    static public function addOrUpdate($search_params, $set_params)
+    {
+        if (isset($search_params) and isset($set_params)) {
+
+            $rec = self::where($search_params)->first();
+
+            if (!isset($rec)) {
+                $rec = new self($search_params);
+            }
+            $rec->fill($set_params);
+            $rec->save();
+
+            return $rec;
+        }
+        return null;
+    }
+
+}
